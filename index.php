6e296f044711e9c0060f7926ab4a9bcc13bf4673
vhholiveira <vitoriagoncalvesoliveira288@gmail.com>
2023-06-14 07:13:36 -0300

    criandotabela

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Sintética</title>
+</head>
+
+<body>
+    <h1>Unidades Federativas do Brasil</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Bandeira</th>
+                <th>Unidade Federativa</th>
+                <th>Abreviação</th>
+                <th>Sede de Governo</th>
+                <th>Área(km²)</th>
+                <th>População(2014) </th>
+                <th>Densidade(2005)</th>
+                <th>PIB(2015)</th>
+                <th>(%Total)(2015)</th>
+                <th>PIB per capita(R$)(2015)</th>
+                <th>IDH(2010)</th>
+                <th>Alfabetização(2016)</th>
+                <th>Mortalidade Infantil(2016)</th>
+                <th>Expectativa de Vida(2016)</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+
+    </table>
+</body>
+
+</html>
